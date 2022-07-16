1d637ad176caf3c1c458bf9f50ff868268ec71e5
David Luna <davidluna.fn@gmail.com>
2022-07-15 22:31:03 -0500

    new encoder model

--- src/models.py
+++ src/models.py
@@ -2,64 +2,35 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class Residual(nn.Module):
-    def __init__(self, in_channels, num_hiddens, num_residual_hiddens):
-        super(Residual, self).__init__()
-        self._block = nn.Sequential(
-            nn.ReLU(True),
-            nn.Conv2d(in_channels=in_channels,
-                      out_channels=num_residual_hiddens,
-                      kernel_size=3, stride=1, padding=1, bias=False),
-            nn.ReLU(True),
-            nn.Conv2d(in_channels=num_residual_hiddens,
-                      out_channels=num_hiddens,
-                      kernel_size=1, stride=1, bias=False)
-        )
-    
-    def forward(self, x):
-        return x + self._block(x)
 
 
-class ResidualStack(nn.Module):
-    def __init__(self, in_channels, num_hiddens, num_residual_layers, num_residual_hiddens):
-        super(ResidualStack, self).__init__()
-        self._num_residual_layers = num_residual_layers
-        self._layers = nn.ModuleList([Residual(in_channels, num_hiddens, num_residual_hiddens)
-                             for _ in range(self._num_residual_layers)])
-
-    def forward(self, x):
-        for i in range(self._num_residual_layers):
-            x = self._layers[i](x)
-        return F.relu(x)
 
 class Encoder(nn.Module):
 
     # docstring for Encoder
-    def __init__(self, in_channels, num_hiddens, num_residual_layers, num_residual_hiddens):
+    def __init__(self, in_channels, num_hiddens):
         super(Encoder, self).__init__()
 
+
         self._conv_1 = nn.Conv2d(in_channels=in_channels,
-                                 out_channels=num_hiddens // 2,
-                                 kernel_size=(5, 5),
+                                 out_channels=num_hiddens // 4,
+                                 kernel_size=(3, 3),
                                  stride=(2, 2), padding=(0, 0))
-        
-        self._conv_2 = nn.Conv2d(in_channels=num_hiddens // 2,
+        self._conv_2 = nn.Conv2d(in_channels=num_hiddens // 4,
                                  out_channels=num_hiddens,
-                                 kernel_size=(4, 4),
+                                 kernel_size=(3, 3),
                                  stride=(2, 2), padding=(0, 0))
 
-        self.residual_stack = ResidualStack(in_channels=num_hiddens,
-                          num_hiddens=num_hiddens,
-                          num_residual_layers=num_residual_layers,
-                          num_residual_hiddens=num_residual_hiddens)
+
+        self.pooling = nn.MaxPool2d(2)
 
     def forward(self, inputs):
-
+        
         x = self._conv_1(inputs)
         x = F.leaky_relu(x)
 
         x = self._conv_2(x)
-        x = self.residual_stack(x)
+        x = F.leaky_relu(x)
 
         return x
 
