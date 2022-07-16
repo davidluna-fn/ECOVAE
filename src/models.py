a50105eccf998ebdba0c43b398ee1b7a1f6c5443
David Luna <davidluna.fn@gmail.com>
2022-07-15 22:37:32 -0500

    model vqvae

--- src/models.py
+++ src/models.py
@@ -182,14 +182,14 @@ class VectorQuantizerEMA(nn.Module):
 
 
 class VQVAE(nn.Module):
-    def __init__(self, num_hiddens, num_embeddings, embedding_dim, commitment_cost, num_residual_layers, num_residual_hiddens, decay=0):
+    def __init__(self, num_hiddens, num_embeddings, embedding_dim, commitment_cost, decay=0):
         super(VQVAE, self).__init__()
 
-        self._encoder = Encoder(1, num_hiddens, num_residual_layers, num_residual_hiddens)
+        self._encoder = Encoder(1, num_hiddens)
         self._pre_vq_conv = nn.Conv2d(in_channels=num_hiddens,
                                       out_channels=embedding_dim,
-                                      padding=0,
-                                      kernel_size=(3, 3),
+                                      padding=(0,0),
+                                      kernel_size=(4, 4),
                                       stride=(2, 2))
         if decay > 0.0:
             self._vq_vae = VectorQuantizerEMA(num_embeddings, embedding_dim,
@@ -197,11 +197,13 @@ class VQVAE(nn.Module):
         else:
             self._vq_vae = VectorQuantizer(num_embeddings, embedding_dim,
                                            commitment_cost)
-        self._decoder = Decoder(embedding_dim,num_hiddens, num_residual_layers, num_residual_hiddens)
+
+        self._decoder = Decoder(embedding_dim,num_hiddens)
 
     def forward(self, x):
         z = self._encoder(x)
         z = self._pre_vq_conv(z)
+
         loss, quantized, perplexity, _ = self._vq_vae(z)
         x_recon = self._decoder(quantized)
 
